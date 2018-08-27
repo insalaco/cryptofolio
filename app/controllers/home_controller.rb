30751ca3efb166bcb0a1f2c8ce5e97cdc10415a7
Michael Insalaco <insalaco01@gmail.com>
2018-08-27 15:31:21 -0400

    connect to coinmarketcap api and loop through results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "EOS", "XRP", "ADA", "STEEM",]
   end
 end
