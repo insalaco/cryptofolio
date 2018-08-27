0656f8cde0283bd8eb621163bb4a09d53839e3ec
Michael Insalaco <insalaco01@gmail.com>
2018-08-27 19:13:12 -0400

    add single lookup for each currency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,26 @@
 class HomeController < ApplicationController
+  require 'net/http'
+  require 'json'
+  
   def index
-    require 'net/http'
-    require 'json'
+
     @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
     @my_coins = ["BTC", "EOS", "XRP", "ADA", "STEEM",]
+  end
+
+  def lookup
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+    
+    @symbol = params[:sym]
+
+    if @symbol
+      @symbol = @symbol.upcase
+    end
   end
 end
